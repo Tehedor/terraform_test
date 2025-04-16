47338445d2239c4ae28759b636da5c03bbdca20a
tehedor <sergiot1805@gmail.com>
2025-04-16 19:04:57 +0200

    terraform init

--- 20_teset_variables/main.tf
+++ 20_teset_variables/main.tf
@@ -2,7 +2,7 @@ resource "local_file" "credentials_file" {
 
   filename = "${path.module}/credentials.txt"
   content = <<EOT
-  Instance Name: ${var.instance_name}
-  Instance pass: ${var.instance_pass}
-  EOT
+Instance Name: ${var.instance_name}
+Instance pass: ${var.instance_pass}
+EOT
 }
